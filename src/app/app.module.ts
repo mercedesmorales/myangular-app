2f00ab05e02c4ee078b43b8216d87f924f2ffff0
LENOVO-PC\Mercedes <mmoraleso@hotmail.com>
2021-02-24 12:18:21 -0500

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { StudentModule } from './student/student.module';
+
+import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student/student.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: StudentComponent
+    }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    StudentModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
